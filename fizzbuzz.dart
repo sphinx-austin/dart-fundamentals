ebae4d98d7ce90fa470ea9c0a45cd5827db382df
sphinx-austin <austinjuniour4@gmail.com>
2023-08-02 18:56:33 +0300

    fizzbuzz

---
+++ fizzbuzz.dart
@@ -0,0 +1,6 @@
+void main() {
+  // Print numbers 1 - 100
+  // If any numbers is divisible by 3, print fizz
+  // If any nimber is divisible by 5, print fuzz
+  // If divisble by both 3 and 5, print fizzbuzz
+}
