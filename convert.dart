8dc1ce8dd2736f75b95017f2e9ddce93d2f81b33
sphinx-austin <austinjuniour4@gmail.com>
2023-08-02 18:35:59 +0300

    string to double

--- convert.dart
+++ convert.dart
@@ -7,4 +7,11 @@ void main() {
   b = "1";
   c = a + int.parse(b);
   print("$a + $b = $c");
+
+  // String to Double
+  var e, d, f;
+  e = "0.1";
+  d = 78;
+  f = d + double.parse(e);
+  print("$d + $e = $f");
 }
